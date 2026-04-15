cf35821e84bc19c4c93de35736c23a313a6d8d3a
Chenye <llchenye341202@gmail.com>
2026-04-15 16:55:14 +0200

    modif les fonctions lecture

--- tme1.py
+++ tme1.py
@@ -5,15 +5,19 @@ def lire_preferences_etu(f) :
     monFichier.close()
     for i in range (0, len(contenu)) :
         contenu[i] = contenu[i].split()
-        contenu[i] = contenu[i][2:]
+        contenu[i] = [int(x) for x in contenu[i][2:]]
     return contenu
 
 def lire_preferences_spe(f) :
     monFichier = open(f, "r")
     contenu = monFichier.readlines()
+
+    ligne_cap = contenu[1].split()
+    capacites = [int(x) for x in ligne_cap[1:]]
+
     contenu = contenu[2:]
     monFichier.close()
     for i in range (0, len(contenu)) :
         contenu[i] = contenu[i].split()
-        contenu[i] = contenu[i][2:]
-    return contenu
+        contenu[i] = [int(x) for x in contenu[i][2:]]
+    return contenu, capacites
